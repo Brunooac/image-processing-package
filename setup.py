b440275a634dc8ed9fc86e0dd3b3ddd4eb86dc6c
Brunooac <brunoandradecarvalho2@gmail.com>
2022-10-19 15:04:57 -0300

    alterações no setup para publicação no pypi

--- setup.py
+++ setup.py
@@ -7,14 +7,13 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="image-processing-package",
-    version="0.0.1",
+    name="bruno-image-processing",
+    version="0.0.2",
     author="Bruno Carvalho",
     author_email="brunoandradecarvalho2@gmail.com",
     description="Pacote cirado com intruções da DIO",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/Brunooac/image-processing-package"
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
